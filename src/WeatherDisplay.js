1c448710b53cbeb93dcd193a9ed936e9d38b6634
Galstyan Artur <aptyp88@gmail.com>
2020-06-18 14:54:47 +0300

    first commit

---
+++ src/WeatherDisplay.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+class WeatherDisplay extends Comment{
+    render(){
+        return(
+            <h1>Displaying some Weather!</h1>
+        );
+    }
+}
+
+export default;
